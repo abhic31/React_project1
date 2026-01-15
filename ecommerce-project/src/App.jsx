ff4e75aa38f6e2ca4d3b03b3c4cbb8f23a683884
AgileBite <agilebite.3@gmail.com>
2026-01-15 18:20:22 +0530

    more changes

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import { Routes,Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 
 function App() {
   return (
-    <HomePage />
+    <Routes>
+      <Route index = "/" element={<HomePage />} />
+      <Route path = "/checkout" element={<div>Test checkout page</div>} />
+    </Routes>
+    
   )
 }
 export default App
