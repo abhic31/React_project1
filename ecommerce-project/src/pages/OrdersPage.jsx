454373f69142dd262ba689eeb7efffd7d0a587cc
AgileBite <agilebite.3@gmail.com>
2026-01-26 15:03:30 +0530

    orders page visibility

--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,12 +1,18 @@
 import { Headers } from '../components/Headers';
 import './orders.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react';
 
-export function OrdersPage() {
+export function OrdersPage({ cart }) {
+
+    const [] = useState();
+
+
     return (
         <>
             <title>Orders</title>
 
-            <Headers />
+            <Headers cart= {cart} />
     
             <div className="orders-page">
             <div className="page-title">Your Orders</div>
